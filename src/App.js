301007f8d86e64d32c31e754b23119a613634299
Julieta Magnago <julietamagnago@gmail.com>
2022-11-09 17:12:27 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { Previewer } from './Previewer/Previewer';
+import { Editor } from './Editor/Editor'
+import { marked } from 'marked';
+import DOMPurify from 'dompurify';
+
+const initialValue = `# Heading level 1 
+## Heading level 2
+I just love **bold text**.
+> Dorothy followed her through many of the beautiful rooms in her castle.
+1. First item
+1. Second item
+1. Third item
+1. Fourth item
+* First item
+* Second item
+* Third item
+* Fourth item
+This is inline code \`<div></div>\`
+This is a block of code
+\`\`\`
+    let x = 1;
+    let y = 2;
+ \`\`\`  
+ ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+My favorite search engine is [Duck Duck Go](https://duckduckgo.com).`
+ 
+marked.setOptions({
+  breaks: true
+})
 
 function App() {
+  const [val, setVal] = useState(initialValue);
+
+  const handleChange = (e) =>{
+    setVal(e.target.value)
+  }
+
+  const toParse = marked.parse(val);
+  const toParseSafe = DOMPurify.sanitize(toParse)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor onChange={handleChange} value={val} />
+      <Previewer value={toParseSafe} />
     </div>
   );
 }
---
+++ src/Previewer/Previewer.js
@@ -0,0 +1,33 @@
+import { marked } from 'marked';
+import './Previewer.css';
+
+
+export function Previewer(props) {
+
+    const renderer = new marked.Renderer()
+
+    return (
+        <div className="previewWrap">
+            <div className="previewMenu">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                </svg>
+                <p>Preview</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-zoom-in" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+                    <path d="M10.344 11.742c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1 6.538 6.538 0 0 1-1.398 1.4z" />
+                    <path fill-rule="evenodd" d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5z" />
+                </svg>
+            </div>
+            <div id="preview" dangerouslySetInnerHTML={{
+                __html: marked(props.value, { renderer: renderer })
+            }}>
+            
+                
+            </div>
+            
+            
+        </div>
+    )
+}
